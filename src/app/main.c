ff6e7abafeefe0a1fbc9d3e564f6f1ec38b470e9
Vladimir Petrigo <vladimir.petrigo@gmail.com>
2024-01-08 00:05:54 +0100

    app: main: Fix build for Win/Linux environment

--- src/app/main.c
+++ src/app/main.c
@@ -6,14 +6,22 @@
 
 #include "temperature.h"
 
+#if defined(_WIN32) || defined(MINGW)
 #include <windows.h>
+#elif defined(__linux__)
+#include <unistd.h>
+#endif
 
 #include <stdio.h>
+
+#if defined(_WIN32) || defined(MINGW)
+#define sleep(x) Sleep(x * 1000)
+#endif
 
 int main(void) {
     for (;;) {
         printf("Periodic temperature reading: %+4d C\r\n", temperature_get());
-        Sleep(2000);
+        sleep(2);
     }
 
     return 0;
